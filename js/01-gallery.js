7b8840483ca777fce15f4ce2cf18451850101c40
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-09-17 06:59:02 +0300

    task1-complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+    </a>
+    </li>`;
+  })
+  .join("");
+
+gallery.addEventListener("click", handleClick);
+let instance = "";
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  //   const { original, description } = galleryItems.find(({ description }) => {
+  //     return description === event.target.getAttribute("alt");
+  //   });
+  const description = event.target.getAttribute("alt");
+  const original = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `
+  <img src="${original}" alt="${description}">
+  `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleClose);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleClose);
+      },
+    }
+  );
+  instance.show();
+}
+
+function handleClose(event) {
+  if (event.key !== "Escape") {
+    return;
+  }
+  instance.close();
+}
