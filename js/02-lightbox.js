c1df2cc701a7790d2ec3d6f6098c0731ee76f1f5
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-09-17 08:27:28 +0300

    end

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+    </a>
+    </li>`;
+  })
+  .join("");
+
+const simpleGallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
